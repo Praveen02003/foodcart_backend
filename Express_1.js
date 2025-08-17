cd86f2916fce35b98e448bb0c0592b6e82c0f2a1
YOUR NAME <YOUR EMAIL@SAMLECOM>
2025-08-17 18:34:43 +0530

    Fix:Changes

--- Express_1.js
+++ Express_1.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json({ limit: "10mb" })); 
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-mongoose.connect('mongodb://127.0.0.1:27017/foodcart', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
